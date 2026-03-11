647e11f8394e0cf19be428eed0305b8c4c5992d2
Justiniscoding <justinlasinski@gmail.com>
2026-03-11 12:26:51 -0400

    feat: create species and speciator classes for usage in NEAT algorithm

--- lib/nn.ts
+++ lib/nn.ts
@@ -191,6 +191,66 @@ class NeuralNetwork {
 
 		return networkOutputs;
 	}
+
+	distanceTo(network2: NeuralNetwork, c1 = 1.0, c2 = 1.0, c3 = 0.4) {
+		let genes1 = new Map<number, NeuralConnection>();
+		let genes2 = new Map<number, NeuralConnection>();
+
+		for (let connection of this.connections) {
+			genes1.set(connection.innovation, connection);
+		}
+
+		for (let connection of network2.connections) {
+			genes2.set(connection.innovation, connection);
+		}
+
+		let innovations1 = new Set(genes1.keys());
+		let innovations2 = new Set(genes1.keys());
+
+		let matching = innovations1.intersection(innovations2);
+		let disjoint = innovations1.symmetricDifference(innovations2);
+
+		let excess = new Set<number>();
+
+		let maxInnovations1 = Math.max(...innovations1);
+		let maxInnovations2 = Math.max(...innovations2);
+		let maxInnovation = Math.min(maxInnovations1, maxInnovations2);
+
+		let oldDisjoint = new Set([...disjoint]);
+
+		for (let innovation of oldDisjoint) {
+			if (innovation > maxInnovation) {
+				excess.add(innovation);
+				disjoint.delete(innovation);
+			}
+		}
+
+		let averageWeightDifference = 0;
+
+		if (matching) {
+			let weightDifference = 0;
+
+			for (let match of matching) {
+				const weight1 = genes1.get(match)?.weight ?? 0;
+				const weight2 = genes2.get(match)?.weight ?? 0;
+				weightDifference += Math.abs(weight1 - weight2);
+			}
+
+			averageWeightDifference = weightDifference / matching.size;
+		} else {
+			averageWeightDifference = 0;
+		}
+
+		let N = Math.max(network1.connections.length, network2.connections.length);
+
+		if (N < 20) {
+			N = 1;
+		}
+
+		const difference = (c1 * excess.size) / N + (c2 * disjoint.size) / N + c3 * averageWeightDifference;
+
+		return difference;
+	}
 }
 
 
@@ -261,6 +321,94 @@ class InnovationTracker {
 	}
 }
 
+class Species {
+	representative: NeuralNetwork;
+	members: NeuralNetwork[];
+
+	adjustedFitness: number = 0
+	bestFitness: number = -Infinity;
+	stagnantGenerations: number = 0
+
+	constructor(representative: NeuralNetwork) {
+		this.representative = representative;
+		this.members = [representative];
+	}
+
+	addMember(network: NeuralNetwork) {
+		this.members.push(network);
+	}
+
+	clearMembers() {
+		this.members = [];
+	}
+
+	updateFitnessStats() {
+		if (this.members.length == 0) {
+			this.adjustedFitness = 0;
+			return;
+		}
+
+		let currentBestFitness = 0;
+		let totalFitness = 0;
+
+		for (let member of this.members) {
+			currentBestFitness = Math.max(member.fitness, currentBestFitness);
+			totalFitness += member.fitness;
+		}
+
+		if (currentBestFitness > this.bestFitness) {
+			this.bestFitness = currentBestFitness;
+			this.stagnantGenerations = 0;
+		} else {
+			this.stagnantGenerations++;
+		}
+
+		this.adjustedFitness = totalFitness / this.members.length;
+	}
+}
+
+class Speciator {
+	species: Species[] = [];
+
+	compatibilityThreshold: number = 3.0;
+
+	constructor(compatibilityThreshold = 3.0) {
+		this.compatibilityThreshold = compatibilityThreshold;
+	}
+
+	speciate(population: NeuralNetwork[]) {
+		for (let species of this.species) {
+			species.clearMembers();
+		}
+
+		for (let network of population) {
+			let foundSpecies = false;
+
+			for (let species of this.species) {
+				if (species.representative.distanceTo(network) < this.compatibilityThreshold) {
+					species.addMember(network);
+					foundSpecies = true;
+					break;
+				}
+			}
+
+			if (!foundSpecies) {
+				const newSpecies = new Species(network);
+				this.species.push(newSpecies);
+			}
+		}
+
+		this.species = this.species.filter(species => species.members.length > 0);
+
+		for (let species of this.species) {
+			species.updateFitnessStats();
+
+			// Get member with most fitness (epic hacker js)
+			species.representative = species.members.reduce((acc, current) => acc.fitness > current.fitness ? acc : current);
+		}
+	}
+}
+
 export class NEAT {
 	population: NeuralNetwork[] = [];
 	innovationTracker: InnovationTracker = new InnovationTracker();
@@ -300,63 +448,4 @@ export class NEAT {
 		}
 	}
 
-	distance(network1: NeuralNetwork, network2: NeuralNetwork, c1 = 1.0, c2 = 1.0, c3 = 0.4) {
-		let genes1 = new Map<number, NeuralConnection>();
-		let genes2 = new Map<number, NeuralConnection>();
-
-		for (let connection of network1.connections) {
-			genes1.set(connection.innovation, connection);
-		}
-
-		for (let connection of network2.connections) {
-			genes2.set(connection.innovation, connection);
-		}
-
-		let innovations1 = new Set(genes1.keys());
-		let innovations2 = new Set(genes1.keys());
-
-		let matching = innovations1.intersection(innovations2);
-		let disjoint = innovations1.symmetricDifference(innovations2);
-
-		let excess = new Set<number>();
-
-		let maxInnovations1 = Math.max(...innovations1);
-		let maxInnovations2 = Math.max(...innovations2);
-		let maxInnovation = Math.min(maxInnovations1, maxInnovations2);
-
-		let oldDisjoint = new Set([...disjoint]);
-
-		for (let innovation of oldDisjoint) {
-			if (innovation > maxInnovation) {
-				excess.add(innovation);
-				disjoint.delete(innovation);
-			}
-		}
-
-		let averageWeightDifference = 0;
-
-		if (matching) {
-			let weightDifference = 0;
-
-			for (let match of matching) {
-				const weight1 = genes1.get(match)?.weight ?? 0;
-				const weight2 = genes2.get(match)?.weight ?? 0;
-				weightDifference += Math.abs(weight1 - weight2);
-			}
-
-			averageWeightDifference = weightDifference / matching.size;
-		} else {
-			averageWeightDifference = 0;
-		}
-
-		let N = Math.max(network1.connections.length, network2.connections.length);
-
-		if (N < 20) {
-			N = 1;
-		}
-
-		const difference = (c1 * excess.size) / N + (c2 * disjoint.size) / N + c3 * averageWeightDifference;
-
-		return difference;
-	}
 }
